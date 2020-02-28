3c82f55135c7459ed97e1e33995db209f1cdd415
Anuj Chaudhari <anujc@vmware.com>
2020-02-27 16:14:00 -0800

    fix header null characters

--- klog.go
+++ klog.go
@@ -629,7 +629,7 @@ func (l *loggingT) formatHeader(s severity, file string, line int) *buffer {
 	buf.tmp[14] = '.'
 	buf.nDigits(6, 15, now.Nanosecond()/1000, '0')
 	buf.tmp[21] = ' '
-	buf.Write(buf.tmp[:30])
+	buf.Write(buf.tmp[:22])
 	buf.WriteString(file)
 	buf.tmp[0] = ':'
 	n := buf.someDigits(1, line)
